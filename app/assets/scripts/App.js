8323ff4780daee30d0ad63d61e5be377eb0313b8
Tonya <tutunja@list.ru>
2018-07-15 19:53:21 +0300

    event modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,8 +2,9 @@ import $ from 'jquery';
 
 import MobileMenu from "./modules/MobileMenu";
 import OpenModal from "./modules/OpenModal";
+import FormFields from "./modules/FormFields";
 
 var mobileMenu = new MobileMenu();
 var openModal = new OpenModal();
-
+var formFields = new FormFields();
  
---
+++ app/assets/scripts/modules/FormFields.js
@@ -0,0 +1,31 @@
+import $ from 'jquery';
+
+class FormFields {
+    constructor() {
+        this.checkboxFinishDate = $("#event__finish-date");
+        this.checkboxFinishTime = $("#event__finish-time");
+        this.fieldFinishDate = $(".form__field--finish-date");
+        this.fieldFinishTime = $(".form__field--finish-time");
+        this.rowDateTime = $(".form__row--finish-date-time");
+        this.toggleVisibility(this.checkboxFinishDate, this.fieldFinishDate, this.rowDateTime);
+        this.toggleVisibility(this.checkboxFinishTime, this.fieldFinishTime, this.rowDateTime);
+    }
+
+    toggleVisibility(checkbox, field, row) {     
+        checkbox.click(function() {
+            field.toggleClass("form__field--is-hidden");
+            //row.removeClass("form__row--is-hidden");
+
+            var checkboxes = checkbox.parents().eq(3).find('input[type="checkbox"]');
+            if (!checkboxes.is(":checked")){
+                row.addClass("form__row--is-hidden");
+            } else {
+                row.removeClass("form__row--is-hidden");
+            }
+
+        });
+    }
+
+}
+
+export default FormFields;
